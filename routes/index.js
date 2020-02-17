8a7b2bda73a6695e83de0f4a96e08d3496c2cd68
chamikagangul <pchamikagangul@gmail.com>
2020-02-17 13:41:47 +0530

    sqlite3 deleted

--- routes/index.js
+++ routes/index.js
@@ -12,23 +12,7 @@ router.get('/', function (req, res, next) {
 });
 
 
-router.get('/create', function (req, res, next) {
-  db.create();
-  console.log("created");
-  res.send("created");
-});
 
-router.get('/fetch', function (req, res, next) {
-  var ds = db.fetch(
-    function (response) {
-      // Here you have access to your variable
-      ds = response
-      console.log(ds);
-      //res.send(ds);
-      res.render('data', { title: 'Data' ,ds:ds});
-    });
-
-});
 
 module.exports = router;
 
